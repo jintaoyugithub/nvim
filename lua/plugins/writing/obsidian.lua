c0b112ce5d069a87cc57a803e9500691e4bc29b8
Jintao Yu <1007936662@qq.com>
2024-09-29 00:21:14 +0200

    finish obisdian.nvim configuration

--- lua/plugins/writing/obsidian.lua
+++ lua/plugins/writing/obsidian.lua
@@ -1,16 +1,6 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    -- lazy = true,
-    -- ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
     dependencies = {
         -- Required.
         "nvim-lua/plenary.nvim",
@@ -18,18 +8,61 @@ return {
         -- see below for full list of optional dependencies 👇
     },
 
+    keys = {
+        { "<leader>oo", "<cmd>ObsidianOpen<cr>",        desc = "Open the notes in Obsidian" },
+        { "<leader>on", ":ObsidianNew ",                desc = "Create a new note" },
+        { "<leader>os", "<cmd>ObsidianSearch<cr>",      desc = "Search Obsidian" },
+        { "<leader>of", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian Find Files" },
+        { "<leader>og", "<cmd>ObsidianTags<cr>",        desc = "a picker list of all occurrences of the given tags" },
+        { "<leader>op", "<cmd>ObsidianPasteImg<cr>",    desc = "Obsidian Paste Image" },
+        { "<leader>or", "<cmd>ObsidianRename<cr>",      desc = "Obsidian Rename current note" },
+        { "<leader>oi", "<cmd>ObsidianTemplate<cr>",    desc = "Insert Obsidian Template into file" },
+        { "<leader>oe", "<cmd>ObsidianExtracNote<cr>",  desc = "extract the visually selected text into a new note and link to it" },
+        { "<leader>od", "<cmd>ObsidianDailies<cr>",     desc = "a picker display daily-notes" },
+        { "<leader>ot", "<cmd>ObsidianToday<cr>",       desc = "open/create a daily note" },
+    },
+
     config = function()
         require("obsidian").setup {
             -- Set up the vaults directory
             dir = "$HOME/vaults/",
-            notes_subdir = "temp-notes",
+            notes_subdir = "inbox",
+
+            -- Optional, customize how note IDs are generated given an optional title.
+            ---@param title string|?
+            ---@return string
+            note_id_func = function(title)
+                -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+                -- In this case a note with the title 'My new note' will be given an ID that looks
+                -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+                local suffix = ""
+                if title ~= nil then
+                    -- If title is given, transform it into valid file name.
+                    suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                else
+                    -- If title is nil, just add 4 random uppercase letters to the suffix.
+                    for _ = 1, 4 do
+                        suffix = suffix .. string.char(math.random(65, 90))
+                    end
+                end
+                return tostring(os.time()) .. "-" .. suffix
+            end,
+
+            -- Optional, customize how note file names are generated given the ID, target directory, and title.
+            ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+            ---@return string|obsidian.Path The full path to the new note.
+            note_path_func = function(spec)
+                -- This is equivalent to the default behavior.
+                local path = spec.dir / tostring(spec.id)
+                return path:with_suffix(".md")
+            end,
 
             -- Set up completion
             completion = {
                 -- Set to false to disable completion.
                 nvim_cmp = true,
-                -- Trigger completion at 2 chars.
-                min_chars = 2,
+                -- Trigger completion at 1 chars.
+                min_chars = 1,
             },
             -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
             -- way then set 'mappings = {}'.
@@ -41,13 +74,6 @@ return {
                     end,
                     opts = { noremap = false, expr = true, buffer = true },
                 },
-                -- Toggle check-boxes.
-                ["<leader>ch"] = {
-                    action = function()
-                        return require("obsidian").util.toggle_checkbox()
-                    end,
-                    opts = { buffer = true },
-                },
                 -- Smart action depending on context, either follow link or toggle checkbox.
                 ["<cr>"] = {
                     action = function()
@@ -57,34 +83,23 @@ return {
                 }
             },
 
-            -- Where to put new notes. Valid options are
-            --  * "current_dir" - put new notes in same directory as the current buffer.
-            --  * "notes_subdir" - put new notes in the default notes subdirectory.
-            new_notes_location = "notes_subdir",
-
-            daily_notes = {
-                -- Optional, if you keep daily notes in a separate directory.
-                folder = "notes/dailies",
-                -- Optional, if you want to change the date format for the ID of daily notes.
-                date_format = "%Y-%m-%d",
-                -- Optional, if you want to change the date format of the default alias of daily notes.
-                alias_format = "%B %-d, %Y",
-                -- Optional, default tags to add to each new daily note created.
-                default_tags = { "daily-notes" },
-                -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-                template = "daily_note_template.md"
-            },
+            -- Optional, boolean or a function that takes a filename and returns a boolean.
+            -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+            disable_frontmatter = true,
 
             -- Optional, alternatively you can customize the frontmatter data.
             ---@return table
             note_frontmatter_func = function(note)
                 -- Add the title of the note as an alias.
-                if note.title then
-                    note:add_alias(note.title)
-                end
+                -- if note.title then
+                --     note:add_alias(note.title)
+                -- end
 
-                -- Set the default meta data here
-                local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+                local out = {
+                    id = note.id,
+                    tags = note.tags,
+                    date = os.date("%Y-%m-%d"),
+                }
 
                 -- `note.metadata` contains any manually added fields in the frontmatter.
                 -- So here we just make sure those fields are kept in the frontmatter.
@@ -97,14 +112,51 @@ return {
                 return out
             end,
 
+            -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+            open_app_foreground = true,
+
             -- Optional, for templates (see below).
             templates = {
                 folder = "templates",
                 date_format = "%Y-%m-%d",
                 time_format = "%H:%M",
                 -- A map for custom variables, the key should be the variable and the value a function
-                substitutions = {},
+                substitutions = {
+                    week_start = function()
+                        local week_ago = os.time() - (7 * 24 * 60 * 60)
+                        return os.date("%Y-%m-%d", week_ago)
+                    end,
+                    month = function()
+                        return os.date("%m")
+                    end,
+                    year = function()
+                        return os.date("%Y")
+                    end,
+                    day = function()
+                        return os.date("%d")
+                    end
+                },
             },
+
+            -- Where to put new notes. Valid options are
+            --  * "current_dir" - put new notes in same directory as the current buffer.
+            --  * "notes_subdir" - put new notes in the default notes subdirectory.
+            new_notes_location = "notes_subdir",
+
+            daily_notes = {
+                -- Optional, if you keep daily notes in a separate directory.
+                folder = "notes/daily_notes",
+                -- Optional, if you want to change the date format for the ID of daily notes.
+                date_format = "%Y-%m-%d",
+                -- Optional, if you want to change the date format of the default alias of daily notes.
+                alias_format = "%B %-d, %Y",
+                -- Optional, default tags to add to each new daily note created.
+                default_tags = { "daily-notes" },
+                -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+                template = "daily.md"
+            },
+
+
 
             -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
             -- URL it will be ignored but you can customize this behavior here.

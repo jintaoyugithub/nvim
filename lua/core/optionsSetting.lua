b182759c7064c8e1af2c6b7312c6e26924c773ff
jintaoyugithub <1007936662@qq.com>
2025-05-29 01:06:05 +0200

    change tab indent

--- lua/core/optionsSetting.lua
+++ lua/core/optionsSetting.lua
@@ -7,8 +7,8 @@ vim.opt.splitright = true
 vim.opt.wrap = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.clipboard = "unnamedplus"
 
